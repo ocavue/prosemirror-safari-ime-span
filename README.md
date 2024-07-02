07ff7b408698a05f724ad942c0d6ebfb96d5d429
ocavue <ocavue@gmail.com>
2024-07-03 09:15:25 +1000

    feat: export `imeSpan`

--- README.md
+++ README.md
@@ -2,6 +2,20 @@
 
 [![NPM version](https://img.shields.io/npm/v/prosemirror-safari-ime-span?color=a1b858&label=)](https://www.npmjs.com/package/prosemirror-safari-ime-span)
 
+This module exports a [ProseMirror] plugin to work around the bug in Safari
+where [IME] text replacement removes empty nodes when the composition finishes.
+See also [prosemirror#934] for more details.
+
+## Usage
+
+```ts
+import { imeSpan } from 'prosemirror-safari-ime-span'
+```
+
 ## License
 
 MIT
+
+[ProseMirror]: https://prosemirror.net/
+[IME]: https://developer.mozilla.org/en-US/docs/Glossary/Input_method_editor
+[prosemirror#934]: https://github.com/ProseMirror/prosemirror/issues/934
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,64 @@
-export const one = 1
-export const two = 2
+import {
+  Plugin,
+  PluginKey,
+  type PluginSpec,
+  type EditorState,
+} from 'prosemirror-state'
+import { Decoration, DecorationSet, type EditorView } from 'prosemirror-view'
+
+import { safari } from './browser'
+
+const key = new PluginKey<boolean>('safari-ime-span')
+
+const spec: PluginSpec<boolean> = {
+  key,
+  state: {
+    init: () => {
+      return false
+    },
+    apply: (tr, value) => {
+      const composing = tr.getMeta(key) as boolean | undefined
+      return composing ?? value
+    },
+  },
+  props: {
+    decorations: createDecorations,
+    handleDOMEvents: {
+      compositionstart: (view) => {
+        view.dispatch(view.state.tr.setMeta(key, true))
+      },
+      compositionend: (view) => {
+        view.dispatch(view.state.tr.setMeta(key, false))
+      },
+    },
+  },
+}
+
+function createDecorations(state: EditorState): DecorationSet | undefined {
+  const { $from, $to, from } = state.selection
+  const composing = key.getState(state)
+  if (composing && $from.sameParent($to)) {
+    const deco = Decoration.widget(from, createSpan, {
+      side: -100,
+      ignoreSelection: true,
+      key: 'safari-ime-span',
+    })
+    return DecorationSet.create(state.doc, [deco])
+  }
+}
+
+function createSpan(view: EditorView): HTMLSpanElement {
+  const span = view.dom.ownerDocument.createElement('span')
+  span.className = 'ProseMirror-safari-ime-span'
+  return span
+}
+
+/**
+ * A plugin as a workaround for a bug in Safari that causes the composition
+ * based IME to remove the empty HTML element with CSS `position: relative`.
+ *
+ * See also https://github.com/ProseMirror/prosemirror/issues/934
+ *
+ * @public @group Plugins
+ */
+export const imeSpan = new Plugin(safari ? spec : { key })
